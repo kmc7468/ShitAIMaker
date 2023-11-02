dc1537a164ad3c36a52a176790d2169c06a5a22a
static <kmc7468@naver.com>
2023-11-02 21:27:51 +0900

    UniqueRef, SharedRef 클래스 파일 분리

--- PALGraphics.cpp
+++ PALGraphics.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cmath>
 #include <stdexcept>
+#include <utility>
 
 void InitializeGraphics() {
 	PALInitializeGraphics();
--- PALGraphics.hpp
+++ PALGraphics.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Reference.hpp"
+
 #include <any>
 #include <cstddef>
 #include <cstdint>
@@ -8,109 +10,8 @@
 #include <optional>
 #include <string>
 #include <string_view>
-#include <type_traits>
-#include <utility>
 #include <variant>
 #include <vector>
-
-template<typename T>
-class UniqueRef {
-	template<typename U>
-	friend class UniqueRef;
-
-private:
-	std::unique_ptr<T> m_Object;
-
-public:
-	UniqueRef(std::unique_ptr<T>&& object) noexcept
-		: m_Object(std::move(object)) {}
-	template<typename U> requires(std::is_base_of_v<T, U>)
-	UniqueRef(UniqueRef<U>&& other) noexcept
-		: m_Object(std::move(other.m_Object)) {}
-	~UniqueRef() = default;
-
-public:
-	template<typename U> requires(std::is_base_of_v<T, U>)
-	UniqueRef& operator=(UniqueRef<U>&& other) noexcept {
-		m_Object = std::move(other.m_Object);
-
-		return *this;
-	}
-	bool operator==(std::nullptr_t) const noexcept {
-		return m_Object == nullptr;
-	}
-	T* operator->() const noexcept {
-		return m_Object.get();
-	}
-	T& operator*() const noexcept {
-		return *m_Object.get();
-	}
-	explicit operator bool() const noexcept {
-		return m_Object != nullptr;
-	}
-
-public:
-	bool IsEmpty() const noexcept {
-		return m_Object == nullptr;
-	}
-	T& Get() const noexcept {
-		return *m_Object.get();
-	}
-};
-
-template<typename T>
-class SharedRef {
-	template<typename U>
-	friend class SharedRef;
-
-private:
-	std::shared_ptr<T> m_Object;
-
-public:
-	SharedRef(std::shared_ptr<T> object) noexcept
-		: m_Object(std::move(object)) {}
-	template<typename U> requires(std::is_base_of_v<T, U>)
-	SharedRef(const SharedRef<U>& other) noexcept
-		: m_Object(other.m_Object) {}
-	template<typename U> requires(std::is_base_of_v<T, U>)
-	SharedRef(SharedRef<U>&& other) noexcept
-		: m_Object(std::move(other.m_Object)) {}
-	~SharedRef() = default;
-
-public:
-	template<typename U> requires(std::is_base_of_v<T, U>)
-	SharedRef& operator=(const SharedRef<U>& other) noexcept {
-		m_Object = other.m_Object;
-
-		return *this;
-	}
-	template<typename U> requires(std::is_base_of_v<T, U>)
-	SharedRef& operator=(SharedRef<U>&& other) noexcept {
-		m_Object = std::move(other.m_Object);
-
-		return *this;
-	}
-	bool operator==(std::nullptr_t) const noexcept {
-		return m_Object == nullptr;
-	}
-	T* operator->() const noexcept {
-		return m_Object.get();
-	}
-	T& operator*() const noexcept {
-		return *m_Object.get();
-	}
-	explicit operator bool() const noexcept {
-		return m_Object != nullptr;
-	}
-
-public:
-	bool IsEmpty() const noexcept {
-		return m_Object == nullptr;
-	}
-	T& Get() const noexcept {
-		return *m_Object.get();
-	}
-};
 
 void InitializeGraphics();
 void FinalizeGraphics() noexcept;
